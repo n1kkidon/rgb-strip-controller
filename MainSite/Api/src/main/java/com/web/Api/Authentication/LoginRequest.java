c108e1e39e3a97dbedfc11434a48a5d550d5cce4
Karolis Krutkevičius <68920983+n1kkidon@users.noreply.github.com>
2023-09-18 21:56:55 +0300

    Delete MainSite directory

--- MainSite/Api/src/main/java/com/web/Api/Authentication/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.web.Api.Authentication;
-
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public class LoginRequest {
-    public String username;
-    public String password;
-}
--- MainSite/Homepage/proxy.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-    {
-        context: ['/api'],
-        logLevel: 'debug',
-        target: 'http://localhost:8080'
-    }
-]
